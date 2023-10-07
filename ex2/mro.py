635dec1cbe5298f1d0d4b0ec6c593ed6bc52f710
rarissimaavis <r.fortino4@studenti.unisa.it>
2023-10-07 17:12:14 +0200

    non so se ha senso

---
+++ ex2/mro.py
@@ -0,0 +1,43 @@
+"""
+Esercizio: Scrivere la classe ClasseB che ha il metodo di istanza contaVarClasse(self,t,n) che
+    – prende in input un tipo t e un intero n
+    – restituisce il numero di variabili di classe di tipo t delle prime n classi nella gerarchia formata dalla classe
+    di cui self e` istanza e dalle sue superclassi, le prime n secondo l’ordine indicato in __mro__ .
+    Se il numero di classi nella suddetta gerarchia è minore di n allora vengono considerate tutte le classi della gerarchia.
+"""
+
+class ClasseB:
+    def contaVarClasse(self, t, n):
+        mro = type(self).__mro__
+        count = 0
+        for cls in mro[:n]:
+            if hasattr(cls, '__dict__'):
+                for nome, tipo in vars(cls).items():
+                    if isinstance(tipo, t) and not nome.startswith('__'):
+                        count += 1
+        return count
+
+
+class A(ClasseB):
+    a = 3
+
+
+class B(A):
+    b = "b"
+
+
+class C(B):
+    c = [1, 2, 3]
+
+
+class D(C):
+    d = "d"
+    e = 3
+
+
+obj = D()
+tipo = str
+n = 5
+count = obj.contaVarClasse(tipo, n)
+print(f"Numero di variabili di classe di tipo {tipo} nelle prime {n} classi: {count}")
+
