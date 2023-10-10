d73aa386048edc0766e70dae0dc75dcafcc57c95
rarissimaavis <r.fortino4@studenti.unisa.it>
2023-10-10 11:32:55 +0200

    class decorator + RecursionError

---
+++ ex4/classdec3.2.py
@@ -0,0 +1,38 @@
+#versione che si spacca (RecursionError)
+
+def count(aClass):
+    aClass.numInstances = 0
+
+    def __newInit__(self, *args, **kwargs):
+        aClass.numInstances += 1
+        aClass.__init__(self, *args, **kwargs)
+
+    aClass.__init__ = __newInit__
+    return aClass
+
+
+@count
+class Spam:
+    pass
+
+
+@count
+class Sub(Spam):
+    pass
+
+
+@count
+class Other(Spam):
+    pass
+
+
+spam = Spam()
+sub = Sub()
+other = Other()
+print("spam", spam.numInstances)
+print("sub", sub.numInstances)
+print("other", other.numInstances)
+other=Other()
+print("other", other.numInstances)
+print("spam", spam.numInstances)
+print("sub", sub.numInstances)
---
+++ ex4/classdec3.py
@@ -0,0 +1,37 @@
+def count(aClass):
+    aClass.numInstances = 0
+    oldInit = aClass.__init__
+
+    def __newInit__(self, *args, **kwargs):
+        aClass.numInstances += 1
+        oldInit(self, *args, **kwargs)
+
+    aClass.__init__ = __newInit__
+    return aClass
+
+
+@count
+class Spam:
+    pass
+
+
+@count
+class Sub(Spam):
+    pass
+
+
+@count
+class Other(Spam):
+    pass
+
+
+spam = Spam()
+sub = Sub()
+other = Other()
+print("spam", spam.numInstances)
+print("sub", sub.numInstances)
+print("other", other.numInstances)
+other=Other()
+print("other", other.numInstances)
+print("spam", spam.numInstances)
+print("sub", sub.numInstances)
