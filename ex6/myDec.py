a7c82541c56f9b01ea3b1a32c4dedb37c5b0213c
rarissimaavis <r.fortino4@studenti.unisa.it>
2023-10-15 11:32:46 +0200

    class decorator (contaVarClasse))

---
+++ ex6/myDec.py
@@ -0,0 +1,27 @@
+"""
+Scrivere un decoratore di classe myDecorator che dota la classe decorata di un metodo di istanza contaVarClasse che
+prende in input un tipo t e restituisce il numero di variabili di classe di tipo t della classe.
+"""
+
+
+def myDecorator(cls):
+    def contaVarClasse(self, t):
+        count = sum(1 for name, value in vars(cls).items() if isinstance(value, t) and not (name.startswith('__') and name.endswith('__')))
+        return count
+
+    cls.contaVarClasse = contaVarClasse
+    return cls
+
+
+@myDecorator
+class myClass:
+    var1 = 10.1
+    var2 = "Hello"
+    var3 = 3.2
+    var4 = "World"
+    var5 = 20
+
+
+obj = myClass()
+print(obj.contaVarClasse(int))
+print(obj.contaVarClasse(str))
