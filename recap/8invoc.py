d5723e528c3507e5805af6e3f787de5fc3da5975
rarissimaavis <r.fortino4@studenti.unisa.it>
2023-10-22 16:26:49 +0200

    fa un po' schifo tbh

---
+++ recap/8invoc.py
@@ -0,0 +1,27 @@
+"""
+Scrivere una classe che contiene un metodo che restituisce il numero di invocazioni degli altri metodi della classe.
+Il codice dei suddetti metodi non deve essere modificato.
+"""
+
+
+class MyClass:
+    count = 0
+
+    def __getattribute__(self, item):
+        if item in ["m1", "m2"]:
+            MyClass.count += 1
+        return super(MyClass, self).__getattribute__(item)
+
+    def m1(self):
+        pass
+
+    def m2(self):
+        pass
+
+
+c = MyClass()
+c.m1()
+c.m2()
+c.m2()
+print(c.count)
+
