4b059292063dbe06950270c343ac3a03ed0bdc19
rarissimaavis <r.fortino4@studenti.unisa.it>
2023-10-25 16:02:04 +0200

    dec max num istanze

---
+++ sparsi/decCls.py
@@ -0,0 +1,34 @@
+"""
+Scrivere il decoratore di classe decoraClasse che trasforma la classe in una classe di cui può esistere al più n istanze.
+Se si tenta di creare più istanze della classe decorata si ha un RuntimeError
+"""
+
+
+def decoraClasse(cls):
+    cls.numInstances = 0
+    cls.__oldInit__ = cls.__init__
+
+    def __newInit__(self, *args, **kwargs):
+        if cls.numInstances == 1:
+            raise RuntimeError
+        else:
+            cls.numInstances = 1
+            cls.__oldInit__(self, *args, **kwargs)
+
+    cls.__init__ = __newInit__
+    return cls
+
+
+@decoraClasse
+class MyClass:
+    def __init__(self, value):
+        self.value = value
+
+
+a = MyClass(2)
+print("prima istanza", a.value)
+try:
+    b = MyClass(3)
+    print("seconda istanza", b.value)
+except RuntimeError:
+    print("no seconda istanza")
---
+++ sparsi/decCls2.py
@@ -0,0 +1,41 @@
+"""
+Scrivere il decoratore di classe decoraClasse che trasforma la classe in una classe di cui può esistere al più un'istanza.
+Se si tenta di creare più di un'istanza della classe decorata si ha un RuntimeError
+"""
+
+
+def decN(n):
+    def decoraClasse(cls):
+        cls.numInstances = 0
+        cls.__oldInit__ = cls.__init__
+
+        def __newInit__(self, *args, **kwargs):
+            if cls.numInstances == n:
+                raise RuntimeError
+            else:
+                cls.numInstances += 1
+                cls.__oldInit__(self, *args, **kwargs)
+
+        cls.__init__ = __newInit__
+        return cls
+    return decoraClasse
+
+
+@decN(n=2)
+class MyClass:
+    def __init__(self, value):
+        self.value = value
+
+
+a = MyClass(2)
+print("prima istanza", a.value)
+try:
+    b = MyClass(3)
+    print("seconda istanza", b.value)
+except RuntimeError:
+    print("no seconda istanza")
+try:
+    c = MyClass(4)
+    print("terza istanza", c.value)
+except RuntimeError:
+    print("no terza istanza")
