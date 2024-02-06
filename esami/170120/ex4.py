f66bcd6d3fdb4dfc0d92858af3e25b041e8b45bf
rarissimaavis <r.fortino4@studenti.unisa.it>
2024-02-06 20:50:38 +0100

    posso splittare i file uwu

---
+++ esami/170120/ex4.py
@@ -0,0 +1,73 @@
+"""
+Scrivere la funzione cerca. Se la funzione ha bisogno di invocare altre procedure, fornire anche quest’ultime.
+La funzione cerca prende in input una lista di stringhe listaParole e una lista di nomi di file listaDiFile,
+e il parametro concorrenza.
+Facendo uso di multiprocessing.JoinableQueue, la funzione cerca deve stampare per ciascuno dei file di listaDiFile
+la parola di listaParole che appare piu` volte nel file.
+Cio` deve essere fatto con un processo separato per ogni file di listaDiFile e le stampe devono essere effettuate
+nell’ordine in cui terminano i processi.
+Il callable usato per effettuare la ricerca nel singolo file deve prendere in input la lista di parole e il nome del file.
+NB: ricordatevi di riposizionare il cursore all’inizio del file con seek(0) ogni volta che cominciate a cercare una nuova parola nel file.
+"""
+import multiprocessing
+
+
+def cercaConc(fileN, listaStringhe):
+    count_dict = {parola: 0 for parola in listaStringhe}
+    ofile = open(fileN, "r")
+    for parola in listaStringhe:
+        ofile.seek(0)
+        testo = ofile.read().split()
+        count_dict[parola] += sum(1 for word in testo if word == parola)
+    max_parola = max(count_dict, key=count_dict.get)
+    return max_parola
+
+
+def cerca(listaDiFile, listaParole, concurrency):
+    jobs = multiprocessing.JoinableQueue()
+    results = multiprocessing.Queue()
+    create_processes(jobs, results, concurrency)
+    add_jobs(listaDiFile, listaParole, jobs)
+    jobs.join()
+
+
+def worker(jobs, results):
+    while True:
+        file, listaParole = jobs.get()
+        result = cercaConc(file, listaParole)
+        if result != "":
+            print(f"La stringa del file {file} che appare per prima nella lista {listaParole} è \"{result}\".")
+        else:
+            print(f"Nessuna parola della lista appare nel file {file}.")
+        jobs.task_done()
+
+
+def create_processes(jobs, results, concurrency):
+    for _ in range(concurrency):
+        process = multiprocessing.Process(target=worker, args=(jobs, results))
+        process.daemon = True
+        process.start()
+
+
+def add_jobs(listaDiFile, listaParole, jobs):
+    for fn in listaDiFile:
+        jobs.put((fn, listaParole))
+
+
+def main():
+    files = ["file1", "file2", "file3", "file4"]
+    parole = ["computer", "very", "with", "it", "algorithms"]
+    cerca(files, parole, 4)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+Ecco cosa deve essere stampato (l'ordine delle righe potrebbe cambiare):
+
+La stringa del file file1 che appare per prima nella lista ['computer', 'very', 'with', 'it', 'algorithms'] è "computer".
+La stringa del file file2 che appare per prima nella lista ['computer', 'very', 'with', 'it', 'algorithms'] è "very".
+La stringa del file file3 che appare per prima nella lista ['computer', 'very', 'with', 'it', 'algorithms'] è "with".
+La stringa del file file4 che appare per prima nella lista ['computer', 'very', 'with', 'it', 'algorithms'] è "it".
+"""
